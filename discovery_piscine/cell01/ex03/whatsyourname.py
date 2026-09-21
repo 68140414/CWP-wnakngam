ba0cb8937ab57fb98f8e686c240b852a0b490b0e
66070185 <66070185@kmitl.ac.th>
2026-09-21 20:23:53 +0700

    Exercise 03: What's your name

---
+++ discovery_piscine/cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+"""Asking what is your name"""
+first_name = input("Hey, what's your first name? : ").strip()
+last_name = input("And your last name? : ").strip()
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
