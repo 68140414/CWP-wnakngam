1102d09b189a33125ac3375986a5691205f64a2c
66070185 <66070185@kmitl.ac.th>
2026-09-22 21:57:01 +0700

    Exercise 00: Up to 25 (Edited)

--- discovery_piscine/cell03/ex00/to25.py
+++ discovery_piscine/cell03/ex00/to25.py
@@ -3,5 +3,6 @@ start = int(input("Enter a number less than 25\n"))
 if start > 25:
     print("Error")
 else:
-    for i in range(start, 26):
-        print(f"Inside the loop, my variable is {i}")
+    while start <= 25:
+        print(f"Inside the loop, my variable is {start}")
+        start += 1
