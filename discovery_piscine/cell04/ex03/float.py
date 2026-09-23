d4c602c3f2c9fc51c8655ba656d27aa76372fecb
66070185 <66070185@kmitl.ac.th>
2026-09-23 16:25:58 +0700

    Exercise 03: Decimal

---
+++ discovery_piscine/cell04/ex03/float.py
@@ -0,0 +1,7 @@
+"""Decimal"""
+num = float(input("Give me a number: "))
+floor_num = int(num)
+if (num == floor_num):
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
