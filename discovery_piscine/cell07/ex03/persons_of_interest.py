62a6eaf906839cf684cfc622e2333e336f00baa7
66070185 <66070185@kmitl.ac.th>
2026-09-23 22:18:24 +0700

    Exercise 03: persons_of_interest

---
+++ discovery_piscine/cell07/ex03/persons_of_interest.py
@@ -0,0 +1,14 @@
+"""Person of Interest"""
+def famous_births(people: dict):
+    """Display Sorted dictionary by birthday"""
+    sorted_dict = sorted(people.values(), key=lambda item: item["date_of_birth"])
+    for person in sorted_dict:
+        print(f"{person['name']} is a great scientist born in {person['date_of_birth']}.")
+
+women_scientists = {
+"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
+famous_births(women_scientists)
